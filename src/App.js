a68278688ffa47347f34d811a29fbce85c38a798
kangaroo19 <86513078+kangaroo19@users.noreply.github.com>
2023-09-30 16:41:21 +0900

    useMemo_2

--- src/App.js
+++ src/App.js
@@ -1,57 +1,36 @@
-import Child from "./Child";
-import Parent from "./Parent";
-import PhotoOne from "./PhotoOne";
-import PhotoTwo from "./PhotoTwo";
-import { useState,useEffect, useMemo } from "react";
+import { useEffect, useMemo, useState } from "react";
 
-const hardCalculate=(number)=>{
-  console.log('어려운 계산')
-  for(let i=0;i<999999999;i++){}
-  return number+10000;
-}
+export default function App() {
+  const [number, setNumber] = useState(0);
+  const [isKorea, setIsKorea] = useState(true);
 
-const easyCalculate=(number)=>{
-  console.log('쉬운 계산')
-  return number+1;
-}
-function App() {
-  const [number,setNumber]=useState(0)
-  const [isKorea,setIsKorea]=useState(true)
-
-  const location=useMemo(()=>{
+  // const location = isKorea ? "한국" : "외국"; =>원시타입
+  const location = useMemo(() => {
     return {
-      country:isKorea ? '한국' : '외국'
-    }
-  },[isKorea]) 
-
-  // const location={
-  //   country:isKorea ? '한국' : '외국'
-  // }
-
-  useEffect(()=>{
-    console.log('useEffect 호출')
-  },[location]) 
-  // setNumber로 인해 number값이 바뀔때 useEffect의 deps는 locatoin이므로 
-  // useEffect 안의 콘솔출력은 안될거 같지만
-  // setNumber로 인해 리랜더링 되면서 객체로 선언된 location값을 참조하는 메모리 주소 바뀜
-  // 그러므로 useEffect 안의 콘솔출력됨
-  // 이를 막기위해 useMemo 사용
-  // useMemo 사용시 useMemo안의 deps값 바뀌지만 않는다면
-  // 이전에 연산한 값 그대로 사용(memoization)
+      country: isKorea ? "한국" : "외국",
+    };
+  },[isKorea]);
+  //location 값이 원시타입일 때는 input필드의 state값 (number)이 바뀌어도 useEffect안의 콘솔이 출력되지않음
+  //number 값이 바뀌면 state값이 바뀌므로 App컴포넌트가 리랜더링되고 isKorea값이 바뀐다면 useEffect의 deps가 location이므로 useEffect의 콘솔 출력됨
+  //location이 원시타입일 경우 number값이 바뀌어도 원시타입에는 값 자체가 담기므로 useEffect의 콘솔 출력되지 않음
+  //location이 객체타입일 경우 number값이 바뀌면 객체타입에는 값 자체가 아닌 메모리 주소가 담긴다
+  //컴포넌트가 리랜더링될때 location을 참조하는 메모리 주소도 같이 바뀌므로 useEffect가 deps의 location도 같이 바뀐 것으로 간주하여 useEffect 콘솔 출력됨
+  //이를 막기위해 location을 useMemo로 감싸 isKorea 값이 바뀌지 않는다면 location 값을 유지하도록 하였음
+  useEffect(() => {
+    console.log("useEffect 호출");
+  }, [location]);
   return (
     <div>
       <h2>하루에 몇끼 먹어요?</h2>
-      <input type="number" value={number} onChange={(e)=>setNumber(e.target.value)}/>
+      <input
+        type="number"
+        value={number}
+        onChange={(e) => setNumber(e.target.value)}
+      />
       <hr />
       <h2>어느 나라에 있어요?</h2>
       <p>나라 : {location.country}</p>
-      <button onClick={()=>setIsKorea(!isKorea)}>비행기 타자</button>
-      <hr />
-      <Child/>
-      <hr />
-      <Parent/>
+      <button onClick={() => setIsKorea(!isKorea)}>비행기 타자</button>
     </div>
-  )
+  );
 }
-
-export default App;
